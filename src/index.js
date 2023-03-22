fce8cecfbb315ae0ce653f5ac241fe66fc7d4691
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-22 22:32:10 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
 function generateCodeFromObject(obj){
     //return a code generated string
    }
