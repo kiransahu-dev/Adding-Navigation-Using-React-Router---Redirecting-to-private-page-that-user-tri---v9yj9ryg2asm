c425825de8c3a6372ab065717d68c493f52f1b27
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-22 22:23:12 +0530

    uncomented and rendered generate code obj and App components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-// function generateCodeFromObject(obj){
-//     //return a code generated string
-//    }
+function generateCodeFromObject(obj){
+    //return a code generated string
+   }
    
 // module.exports=generateCodeFromObject;
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App />,document.getElementById("root"));
