17ed514a0e30fd2c4392aa67c6f19e6cd40e7b26
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-22 21:59:31 +0530

    nav-bar routing commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import NoMatch from "./NoMatch";
 import '../styles/App.css';
+import LocationDisplay from "./LocationDisplay";
 
 class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
+    render() {
+        return (
+            <BrowserRouter>
+                <div id="main">
+                    <LocationDisplay />
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/about'}>About</Link>
+
+                    <Switch>
+                        <Route exact path={'/'} component={Home} />
+                        <Route exact path={'/about'} component={About} />
+                        <Route exact path={'/*'} component={NoMatch} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         )
     }
 }
